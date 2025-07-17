d4b286955823efda553ea77b495f715916ff476c
Ryan Moulaoui <ryan.moulaoui@laposte.net>
2025-07-17 15:43:59 +0200

    Sécurisation htaccess

--- Site_projet/admin/login.php
+++ Site_projet/admin/login.php
@@ -1,7 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 
-require_once '../envloader.php';
+
+require_once __DIR__ . '/../../envloader.php';
+
 $env = charger_env();
 
 $identifiant_admin = $env['ADMIN_USERNAME'];
